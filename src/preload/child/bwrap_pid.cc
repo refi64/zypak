624a61bf325f5610490b1f7cd451cdcb6f252713
Ryan Gonzalez <rymg19@gmail.com>
2020-08-01 18:29:22 -0500

    preload/child: Make getpid work when strace'd
    strace will be the top PID, so the getpid override wouldn't apply.

--- src/preload/child/bwrap_pid.cc
+++ src/preload/child/bwrap_pid.cc
@@ -3,17 +3,51 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unistd.h>
+
+#include <atomic>
+#include <fstream>
+#include <string>
+
 #include "base/base.h"
 #include "preload/declare_override.h"
 
 // If this process is sandboxed, pretend it's PID 1 to pass the sandbox client check.
 
+namespace {
+
+enum class State { kUnknown, kFake, kNoFake };
+
+std::atomic<State> state(State::kUnknown);
+
+}  // namespace
+
 DECLARE_OVERRIDE(pid_t, getpid) {
   auto original = LoadOriginal();
 
   pid_t res = original();
-  if (res == 2 && getenv("SBX_CHROME_API_PRV")) {
-    res = 1;
+
+  // NOTE: This is technically racy, but the worst case scenario is just that this is run twice.
+  if (getenv("SBX_CHROME_API_PRV")) {
+    if (state.load() == State::kUnknown) {
+      if (res == 2) {
+        state = State::kFake;
+      } else {
+        // Check if the parent is strace, if so still override the value.
+        std::string path("/proc/"s + std::to_string(getppid()) + "/comm");
+        std::ifstream is(path);
+        std::string line;
+        if (is && std::getline(is, line) && line == "strace") {
+          state = State::kFake;
+        } else {
+          state = State::kNoFake;
+        }
+      }
+    }
+
+    if (state.load() == State::kFake) {
+      return 1;
+    }
   }
 
   return res;
