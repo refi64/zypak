17df52513899b84c1c3471afde38f7a2a4824314
Ryan Gonzalez <rymg19@gmail.com>
2020-12-19 18:09:25 -0600

    Add some missing license headers

--- src/preload/child/mimic_strategy/initialize.cc
+++ src/preload/child/mimic_strategy/initialize.cc
@@ -1,4 +1,8 @@
-#include "preload/child/mimic_strategy/urandom_fd.h"
+// Copyright 2020 Endless Mobile, Inc.
+// Portions copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "preload/main_override.h"
 
 using namespace zypak::preload;
--- src/preload/child/mimic_strategy/open_urandom.cc
+++ src/preload/child/mimic_strategy/open_urandom.cc
@@ -1,3 +1,8 @@
+// Copyright 2020 Endless Mobile, Inc.
+// Portions copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <fcntl.h>
 #include <sys/syscall.h>
 #include <unistd.h>
